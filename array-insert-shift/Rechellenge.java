f5f0eb37b9dbb71896a7e834fe8cd28805891fb9
shathasmadi <alsmadishatha@gmail.com>
2021-07-28 09:23:23 +0300

    resolve lab 2

--- array-insert-shift/Rechellenge.java
+++ array-insert-shift/Rechellenge.java
@@ -1,16 +1,40 @@
-import java.util.*;
-class Rechallenge {
-    public static void main(String[] args) {
-        insertShiftArray();
-    }
-    public static void insertShiftArray  (){
-        List<Integer> myList = Arrays.asList( 2,4,6,-8);
-        ArrayList<Integer> myArray = new ArrayList<Integer>();
-        myArray.addAll(myList);
-        System.out.println("original Array: "+myArray);
-        int myNum = 5;
-        myArray.add(2,myNum);
-        System.out.println("Output: " +myArray);
 
-    }
-}
+import java.util.Arrays;
+class Rechallenge {
+    
+
+ 
+    
+            static int[] testArr = { 1, 2, 3, 4, 5 };
+    
+            public static void main(String[] args) {
+                insertShiftArray(testArr, 10);
+            }
+    
+            public static void insertShiftArray(int[] arr, int num) {
+                int[] newArr = new int[arr.length + 1];
+    
+                for (int i = 0; i < newArr.length; i++) {
+                    if (arr.length % 2 == 0) {
+                        if (i < arr.length / 2) {
+                            newArr[i] = arr[i];
+                        } else if (i == arr.length / 2) {
+                            newArr[i] = num;
+                        } else {
+                            newArr[i] = arr[i - 1];
+                        }
+                    } else {
+                        if (i < arr.length / 2 + 1) {
+                            newArr[i] = arr[i];
+                        } else if (i == arr.length / 2 + 1) {
+                            newArr[i] = num;
+                        } else {
+                            newArr[i] = arr[i - 1];
+                        }
+                    }
+                }
+    
+                System.out.println("old Array: " + Arrays.toString(arr));
+                System.out.println("new Array: " + Arrays.toString(newArr));
+            }
+        }
