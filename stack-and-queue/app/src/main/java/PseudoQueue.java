0fb4904b7b27e191d9dbfafa58d6c5e40df65f23
shathasmadi <alsmadishatha@gmail.com>
2021-08-08 23:24:41 +0300

    challenge 11

---
+++ stack-and-queue/app/src/main/java/PseudoQueue.java
@@ -0,0 +1,35 @@
+import stack.and.queue.Stack;
+
+public class PseudoQueue <T>{
+
+    public  Stack<T> stack1 = new Stack<T>();
+    public Stack<T> stack2 = new Stack<T>();
+
+
+    public void enQueue(T value)
+    {
+        while (!stack1.isEmpty())
+        {
+            stack2.push(stack1.pop());
+
+        }
+        stack1.push(value);
+        while (!stack2.isEmpty())
+        {
+            stack1.push(stack2.pop());
+        }
+    }
+
+
+     public T deQueue()
+    {
+        if (stack1.isEmpty())
+        {
+            System.out.println("Queue is Empty");
+            System.exit(0);
+        }
+        T value = stack1.peek();
+        stack1.pop();
+        return value;
+    }
+}
