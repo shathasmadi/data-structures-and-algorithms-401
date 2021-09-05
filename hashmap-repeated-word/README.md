cde0a6b8b203080edb90407aa2a19cf84e083575
shathasmadi <alsmadishatha@gmail.com>
2021-09-05 19:18:20 +0300

    solve challenge 31

---
+++ hashmap-repeated-word/README.md
@@ -0,0 +1,13 @@
+# Challenge Summary
+
+In this challenge, the input is a  String. It could be something like a paragraph , larger. The challenge is to return the first word which occurs in the string more than once.
+
+## Whiteboard Process
+![image](challenge31.jpg)
+
+## Approach & Efficiency
+Time:Big O(n^2)
+Space:Big O(n)
+
+## Solution
+![image](repeatedResult.png)
--- hashtable/app/src/main/java/hashtable/App.java
+++ hashtable/app/src/main/java/hashtable/App.java
@@ -14,14 +14,19 @@ public class App {
         System.out.println(new App().getGreeting());
 
 
-
-        HashTable test = new HashTable(10);
-
-        test.add(0,"test adding");
+//
+//        HashTable test = new HashTable(10);
+//
+//        test.add(0,"test adding");
 //        System.out.println(Arrays.toString(test.hash));
 //        System.out.println(test.hash[0].value.toString());
-        System.out.println(test.get(0));
+//        System.out.println(test.get(0));
 
+
+
+        HashRepeated hash =new HashRepeated();
+        String hashWords ="\" It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...\"";
+        System.out.println( hash.repeatedWord(hashWords));
 
     }
 }
